d5959d14028e3291dcad9594e6d56ef44ffc1eb5
unknown <karlsamaha@hotmail.com>
2021-02-08 22:26:09 +0300

    push3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-
+import React, { useEffect, useState } from "react";
 const App = () => {
+ const [tasks,setTasks] = useState([
+{
+    id:1,
+    text:'Doctors Appointment',
+    day:'Feb 5th at 2:30pm',
+    reminder:true,
+} ,
+{
+ id:2,
+ text:'Doctors Appointment',
+ day:'Feb 6th at 2:30pm',
+ reminder:true,
+} ,
+{
+ id:3,
+ text:'Doctors Appointment',
+ day:'Feb 5th at 1:30pm',
+ reminder:false,
+} 
+ ])
+ 
+const deleteTask = (id) =>{
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className = 'container'>
       <Header title="karl"/>
-      <Tasks/>
+      <Tasks tasks={tasks} onDelete = {deleteTask}/>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,37 +1,14 @@
 import React, { useEffect, useState } from "react";
-
-const Tasks = () => {
-
-    const [tasks,setTasks] = useState([ {
-        id:1,
-        text:'Doctors Appointment',
-        day:'Feb 5th at 2:30pm',
-        reminder:true,
-    } ,
-    {
-     id:2,
-     text:'Doctors Appointment',
-     day:'Feb 6th at 2:30pm',
-     reminder:true,
- } ,
- {
-     id:3,
-     text:'Doctors Appointment',
-     day:'Feb 5th at 1:30pm',
-     reminder:false,
- } 
-]
- )
-
+import Task from './Task';
+const Tasks = ({tasks , onDelete }) => {
     return (
         <>
 {
     tasks.map((task) => 
-        <h3 key={task.id}>{task.text}</h3>
+       <Task key={task.id} task={task}
+       onDelete={onDelete}/>
+    )
+}       </>
     )
 }
-        </>
-    )
-}
-
 export default Tasks
